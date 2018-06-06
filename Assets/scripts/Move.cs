34ce2eadce190708e185a35175bfe2953b071c83
H0uS2010 <H0uS2010>
2018-06-06 10:41:22 +0300

    test

---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float speed = 0.2f;
+    public float distance = 10;
+	
+	void Start ()
+    {
+        Debug.Log(speed);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.x >= distance || transform.position.x <= -distance) 
+        {
+            speed = -speed;
+        }
+        transform.Translate(speed, 0, 0);
+
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 50f;
+    public float rotSpeed = 5000f;
+    public float jumpSpeed = 30;
+
+
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float movZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotY = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        float movY = 0;
+        if (Input.GetButtonDown("Jump"))
+        {
+            movY = jumpSpeed * Time.deltaTime;
+        }
+        transform.Translate(movX, movY, movZ);
+        transform.Rotate(0, rotY, 0);
+    }
+    
+        private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("You Dead");
+            GetComponent<Renderer>().material.color = Color.red;
+            Time.timeScale = 0;
+        }
+    }
+}
+
